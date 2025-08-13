1484b11b2b83522f0ec390331763d1bcf8e37008
Dmitrij <chebotarev@dmitry.by>
2025-08-13 14:29:02 +0500

    часть шесть

---
+++ src/Adapter/Main.java
@@ -0,0 +1,11 @@
+package Adapter;
+
+public class Main {
+    public static void main(String[] args) {
+        Adaptee adaptee = new Adaptee();
+
+        Target target = new Adapter(adaptee);
+
+        target.request();
+    }
+}
